5d6a7856875a44a3c9e10157fa6f9bc260ee3f8d
sidin <officialzedro@gmail.com>
2023-10-06 19:42:16 +0530

    created profile page not full

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./page/Home/Home";
+import Profile from "./page/Profile/Profile";
 
 function App() {
   return (
     <>
-        <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<h3>this is Profile PAge </h3>} />
+        <Route path="/profile" element={<Profile />} />
         <Route
           path="/create-blog"
           element={<h3>this is Upload Your BLOG Content PAge</h3>}
+        />
+
+        <Route path="/login" element={<h3>this is login page </h3>} />
+
+        <Route
+          path="/signup"
+          element={<h3>This is Signup page </h3>}
         />
       </Routes>
     </>
