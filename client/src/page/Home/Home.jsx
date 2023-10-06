6c3eff1c6da8c7499da26f3003a723f01d01b878
sidin <officialzedro@gmail.com>
2023-10-06 19:10:18 +0530

    changed something i will update on readme file later

--- client/src/page/Home/Home.jsx
+++ client/src/page/Home/Home.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
-
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 
-
 function Home() {
+  const [search , setSearch] = useState('')
+  const handleSearch = (event)=>{
+    setSearch(event.target.value)
+  }
+  
+
   return (
     <Container>
        <Form className="d-flex col-md-8 mt-4">
               <InputGroup>
-                <Form.Control type="text" placeholder="Search..." />
+                <Form.Control type="text" placeholder="Search..." onChange={handleSearch} />
                 <Button variant="btn btn-success">Search</Button>
               </InputGroup>
             </Form>
